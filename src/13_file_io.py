a4019f66aaa5c5ef08be5e552e2413b2ec329f06
tactnician <tactnician@gmail.com>
2019-07-09 22:43:33 -0400

    completed 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,11 +8,26 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+# f = open('foo.txt', 'r')
+# read_data = f.read()
+# print(read_data)
+# f.close()
+# f.closed
+
+with open('foo.txt') as f:
+    read_data = f.read()
+    print(read_data)
+
+f.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+    f.write(' Line 1\n Line 2 \n Line 3')
+
+with open('bar.txt') as f: 
+    read_data = f.read()
+    print(read_data)
