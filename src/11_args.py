195216188ef370cfc737fe19ea7b1a1d11f5e688
tactnician <tactnician@gmail.com>
2019-07-09 21:55:54 -0400

    completed 11

--- src/11_args.py
+++ src/11_args.py
@@ -4,33 +4,62 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+def f1(n1, n2):
+    return n1 + n2
 
-print(f1(1, 2))
+# print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+def f2(*args):
+    added = 0 
+    # print('f2')
+    # print(*args)
+    # new_list = list(*args)
+    # print(new_list)
+    # return args
+    for v in args: 
+        # print(f"val = {v}")
+        added += v
+    return added
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
-print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
+
+# print(f2(1))                    # Should print 1
+# print(f2(1, 3))                 # Should print 4
+# print(f2(1, 4, -12))            # Should print -7
+# print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+# print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+# def f3(*args):
+#     if len(args) == 1:
+#         for v in args:
+#             return v+1
+#     else:
+#         x = 0
+#         for v in args:
+#             x += v 
+#         return x 
 
-print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+def f3(x, y=1):
+    added = 0
+    if x:
+        added += x
+    if y: 
+        added += y
+    return added
+
+
+# print(f3(1, 2))  # Should print 3
+# print(f3(8))     # Should print 9
 
 
 # Write a function f4 that accepts an arbitrary number of keyword arguments and
@@ -41,7 +70,12 @@ print(f3(8))     # Should print 9
 #
 # Google "python keyword arguments".
 
-# YOUR CODE HERE
+def f4(**kwargs):
+    for k,v in kwargs.items():
+        print(f'key: {k}, value: {v}')
+
+
+
 
 # Should print
 # key: a, value: 12
@@ -60,4 +94,5 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
+
